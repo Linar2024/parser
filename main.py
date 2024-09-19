6e7f0d7d3c4ff12a31ad740d0c80751f08689d86
myau7 <mondi32323@gmail.com>
2024-09-19 14:47:14 +0500

    Add main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+url = "https://www.nasa.gov/news/all-news/"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+news = soup.find_all("div", {"class": "hds-content-item"})
+file = open('news.csv', 'a', encoding='utf-8')
+def parser():
+    for n in news:
+        print(n.find("div", {"class": "hds-content-item-inner"}).get_text().strip())
+        file.write(n.find("div", {"class": "hds-content-item-inner"}).get_text().strip())
+        file.write('/n')
+parser()
